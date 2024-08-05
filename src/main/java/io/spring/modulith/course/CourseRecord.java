820017df787129eb79f9d95d5ea8a3a19465fb9e
Keith Kester <keith.a.kester.ctr@army.mil>
2024-08-05 10:23:18 -0500

    Added archunit test

--- src/main/java/io/spring/modulith/course/CourseRecord.java
+++ src/main/java/io/spring/modulith/course/CourseRecord.java
@@ -1,4 +1,9 @@
 package io.spring.modulith.course;
 
 public record CourseRecord(Long id, String name, Integer level) {
+    public CourseRecord {
+        if (level < 100 || level > 499) {
+            throw new IllegalArgumentException("'level' must be between 100 and 499");
+        }
+    }
 }
